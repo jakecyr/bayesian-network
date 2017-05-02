f7555da91e124069758ac00143704c8d0d29c8ea
Jake <jake@Jakes-MacBook-Air.local>
2017-05-02 15:05:43 -0400

    Add movie recommender example

---
+++ examples/movie_recommender.js
@@ -0,0 +1,17 @@
+var fs = require("fs");
+const csv = require("csvtojson");
+
+var BayesianNetwork = require("../bayesian_network");
+var classifier = new BayesianNetwork();
+
+fs.readFile("../data/movie_ratings.csv", function(err, result){
+
+    csv({noheader:false}).fromString(result.toString())
+    .on('csv',(row)=>{ 
+        if(row.length > 0) classifier.addDocument([row[0], row[1], row[4].split("|").join(" ")], row[2]);
+    })
+    .on('done',()=>{
+        classifier.calculateLogFrequencies();
+        console.log(classifier.classify(['1', '31', 'Drama']));
+    });
+});
