dfe613918f2d3e31d14cba2501f2d1754a9a8721
Jake <jake@Jakes-MacBook-Air.local>
2017-05-02 14:28:51 -0400

    Add examples folder and examples README file

---
+++ examples/README.md
@@ -0,0 +1,3 @@
+# Bayesian Network Examples
+
+**natural_disaster.js**  - trains on tweets from during natural disasters that specify if the tweet is "on-topic" or "off-topic". A possible application could be helping to find those who need assistance during a natural disaster.
---
+++ examples/natural_disaster.js
@@ -0,0 +1,21 @@
+var fs = require("fs");
+const csv = require("csvtojson");
+
+var BayesianNetwork = require("../bayesian_network");
+var classifier = new BayesianNetwork();
+
+// Predict if a tweet is about a natural disaster or not
+fs.readFile("../data/natural_disaster.csv", function(err, result){
+
+    csv({noheader:false}).fromString(result.toString())
+    .on('csv',(row)=>{ 
+        if(row.length > 0) classifier.addDocument(row[1], row[2]);
+    })
+    .on('done',()=>{
+        classifier.calculateLogFrequencies();
+        console.log(classifier.classify("Thoughts and prayers go out to those who were involved in the fertilizer plant explosion in West, TX."));
+        console.log(classifier.classify("Happy to see everyone in Calgary pulling together as community and staying safe through this disaster. #albertaflooding"));
+        console.log(classifier.classify("@wjjenn haha, oh right! You did mention that before. Gel capsules will work just as well as liquid form."));
+        console.log(classifier.classify("Anyone inspired to commit violence after playing Mass Effect 3 is even more unwell than if he played a different game"));
+    });
+});
